3e2bb1bf91d5e2b33a2a04b05f9eaf10825158ca
aravagar <aravagar@umich.edu>
2019-09-19 18:37:07 -0400

    Finished project0

---
+++ project0.py
@@ -0,0 +1,9 @@
+def encrypt_neat(plaintext):
+    ciphertext = ""
+    for character in plaintext.lower():
+        ciphertext += chr((25-(ord(character)-97))%26 + 97)
+    print(ciphertext)
+    return ciphertext
+
+plaintext = input("What would you like to 'encrypt'?")
+ciphertext = encrypt_neat(plaintext)
